32daabecd6eda879e71c41ab17425483f8924317
butlariz <laris.santana09@gmail.com>
2018-10-04 10:37:06 -0300

    configurações do firebase

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyDEdS_fKROrkQ8_0VB90uJvmoXoGAeM760",
+    authDomain: "peixinho-urbano.firebaseapp.com",
+    databaseURL: "https://peixinho-urbano.firebaseio.com",
+    projectId: "peixinho-urbano",
+    storageBucket: "peixinho-urbano.appspot.com",
+    messagingSenderId: "587100445935"
+  };
+  firebase.initializeApp(config);
