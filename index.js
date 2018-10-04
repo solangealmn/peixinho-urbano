eeebaea9c65a71b697e384610e800b9e42d15e95
solangealmn <solangealmn@gmail.com>
2018-10-04 17:55:41 -0300

    Uniformização de navbar e customização de modal.

--- index.js
+++ index.js
@@ -9,15 +9,19 @@ const dataTickets = () => {
       var childKey = childSnapshot.key;
       var childData = childSnapshot.val();
       $(".tickets-list").prepend(`
-          <div class="card" data-task-id=${childKey}>
-            <img class="card-img-top" src=".../100px180/" alt="Card image cap">
-            <div class="card-body">
-              <h5 class="card-title">${childData.name}</h5>
-              <p class="card-text">${childData.product}</p>
-              <p class="card-text">${childData.discount}</p>
-              <p class="card-text">${childData.quantity}</p>
+        <div class="col-4 mb-3">
+          <div class="card-group">
+            <div class="card" data-task-id=${childKey}>
+              <img class="card-img-top" src="dist/assets/comida-natural.jpg" alt="medicina natural">
+              <div class="card-body">
+                <h5 class="card-title">${childData.name}</h5>
+                <p class="card-text">${childData.product}</p>
+                <p class="card-text">${childData.discount}</p>
+                <p class="card-text">${childData.quantity}</p>
+            </div>
+            <button data-get-id=${childKey} class="btn btn-success" data-toggle="modal" data-target="#tickets"> Ver mais </button>
           </div>
-          <button data-get-id=${childKey} class="btn btn-details" data-toggle="modal" data-target="#tickets"> Ver mais </button>
+          </div>
         </div>`
       );
       $(".btn-details").click(function() {
