aa2f4159a6188280eab592ccd4be1fa40ad84334
Danielle Boczkowski <38707471+DanielleBK@users.noreply.github.com>
2018-10-04 16:10:06 -0300

    .

--- index.js
+++ index.js
@@ -10,17 +10,47 @@ const dataTickets = () => {
     snapshot.forEach(function(childSnapshot) {
       var childKey = childSnapshot.key;
       var childData = childSnapshot.val();
+
       $(".tickets-list").prepend(`
-        <li>
+        <div class="col-4">
           <div data-task-id=${childKey} />
+<<<<<<< HEAD
             <span>${childData.name}</span>
             <span>${childData.product}</span>
             <span>${childData.discount}</span>
             <span>${childData.quantity}</span>
-            <button id="details" class="btn"> Ver mais </button>
+            <button data-get-id=${childKey} class="btn btn-details" data-toggle="modal" data-target="#tickets"> Ver mais </button>
+=======
+            <h3 class="h3">${childData.name}</h3>
+            <h4>${childData.product}</h4>
+            <h4 class="h4">${childData.discount}</h4>
+            <h5 class="h5">${childData.quantity}</h5>
+>>>>>>> b8af1fa5942065ba2a671cc14f83f1dc24593ede
           </div>
-        </li>`
+        </div>`
       );
+      $(".btn-details").click(function() {
+        modal(childKey)
+      })
+     });
+  });
+}
+
+function modal(id){
+
+  database.ref("tickets/").once('value')
+  .then(function(snapshot) {
+    snapshot.forEach(function(childSnapshot) {
+      var childKey = childSnapshot.key;
+      var childData = childSnapshot.val();
+      var childData = childSnapshot.val();
+
+      if(id === childKey) {
+        $("#title-modal").text(childData.name);
+        $("#body-modal").text(childData.product);
+        $("#cupom-body-modal").text(childData.code)
+
+      }
      });
   });
 }
