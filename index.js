a0164504309963a64477c06af601f9f1b598c519
solangealmn <solangealmn@gmail.com>
2018-10-04 14:39:45 -0300

    Carrosel funcionando.

--- index.js
+++ index.js
@@ -12,16 +12,12 @@ const dataTickets = () => {
       var childData = childSnapshot.val();
       console.log(childSnapshot.val())
       $(".tickets-list").prepend(`
-        <div class="container">
-          <div class="row">
-            <div class="col-4">
-            <div data-task-id=${childKey} />
-              <h3 class="h3"><span>${childData.name}</span></h3>
-              <span>${childData.product}</span>
-              <span>${childData.discount}</span>
-              <span>${childData.quantity}</span>
-            </div>
-            </div>
+        <div class="col-4">
+          <div data-task-id=${childKey} />
+            <h3 class="h3">${childData.name}</h3>
+            <h4>${childData.product}</h4>
+            <h4 class="h4">${childData.discount}</h4>
+            <h5 class="h5">${childData.quantity}</h5>
           </div>
         </div>`
       );
